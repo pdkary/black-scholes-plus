8e066c02b1c4494d9ff3fc918080e58a31526ea9
pdkary <pdkary@gmail.com>
2021-02-21 18:16:37 -0500

    moved data collection to driver file

---
+++ collect_data.py
@@ -0,0 +1,7 @@
+from src.data_collector import DataCollector
+
+tickers = ['AAPL', 'AMD', 'AMGN', 'AMZN', 'CSCO', 'FB', 'GOOG','IBM', 'INTC', 'MSFT', 'MU', 'NFLX', 'NVDA', 'SHOP']
+filename = "collected_data.csv"
+    
+dc = DataCollector(tickers,interval,filename,.012)
+dc.update()
